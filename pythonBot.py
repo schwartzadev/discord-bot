0f904fd93f60f44a1c0b68387f2f08ccc280d5f1
Andrew Schwartz <andrew@aschwartz.me>
2017-06-05 20:43:55 -0600

    add shib command, clean up...
     embed images
    - rm useless imports
    - rm old xkcd methods
    - add shib class
    - move github, discord keys to config.json
    - add aliases
     Please enter the commit message for your changes. Lines starting

--- pythonBot.py
+++ pythonBot.py
@@ -3,25 +3,28 @@
 import json
 from random import randint
 
-import io
 import aiohttp
 import asyncio
 import async_timeout
 
 import discord
 from discord.ext.commands import Bot
-import aiohttp
 
 
 print('running...')
 my_bot = Bot(command_prefix = '!!')
 
+with open('config.json') as configFile: # import config
+    config = json.load(configFile)
+
+
+
 @my_bot.command(pass_context=True)
 async def hello(ctx):
     """say hi!"""
     print('hello')
+    await my_bot.say(ctx.message.author.mention + ' Hello, world!')
     await my_bot.delete_message(ctx.message)
-    return await my_bot.say('Hello, world!')
 
 
 async def fetch(session, url): # gets a url to text asynchronously
@@ -31,27 +34,18 @@ async def fetch(session, url): # gets a url to text asynchronously
 
 class Comic: # for xkcd command
     totalComics = 0
-    loop = None
 
     async def getImgUrl(self, num):
-        async with aiohttp.ClientSession(loop = self.loop) as session:
+        async with aiohttp.ClientSession() as session:
             html = await fetch(session, 'https://xkcd.com/' + str(num) + '/info.0.json')
             d = json.loads(html)
             return d['img']
-
-    async def saveImg(self, url):
-        with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                test = await resp.read()
-                with open("file/xkcd.png", "wb") as f:
-                    f.write(test)
-                    return f
 
     def randomComicNum(self):
         return randint(1, self.totalComics)
 
     async def setCount(self):
-        async with aiohttp.ClientSession(loop = self.loop) as session:
+        async with aiohttp.ClientSession() as session:
             html = await fetch(session, 'https://xkcd.com/info.0.json')
             d = json.loads(html)
             self.totalComics = d['num']
@@ -59,30 +53,29 @@ class Comic: # for xkcd command
     def getRandomComic(self):
         return self.getImgUrl(self.randomComicNum())
 
-@my_bot.command(pass_context=True)
+c = Comic()
+my_bot.loop.run_until_complete(c.setCount())
+@my_bot.command(pass_context=True, aliases=['x'])
 async def xkcd(ctx, *args):
-    """gets an xkcd comic. optional number following the command"""
-    print('xkcd')
-    c = Comic()
-    c.loop = asyncio.get_event_loop()
-    await c.setCount()
+    """(x) gets an xkcd comic. optional number following the command"""
     if args:
         try:
             n = int(args[0])
         except Exception as e:
-            return await my_bot.say('`Error: args must be a number`')
+            return await my_bot.say(ctx.message.author.mention + ' `Error: args must be a number`')
     else:
         n = c.randomComicNum()
     url = await c.getImgUrl(n)
     # await my_bot.say('`XKCD comic #' + str(n) + ':`')
     em = discord.Embed()
-    await my_bot.send_message(ctx.message.channel, content='`XKCD comic #' + str(n) + ':`', embed=em.set_image(url=url))
+    await my_bot.send_message(ctx.message.channel, content=ctx.message.author.mention + ' `XKCD comic #' + str(n) + ':`', embed=em.set_image(url=url))
     await my_bot.delete_message(ctx.message)
+    print('xkcd #' + str(n))
 
 
-@my_bot.command(pass_context=True)
+@my_bot.command(pass_context=True, aliases=['i'])
 async def iconify(ctx, *args):
-    """turns text into emoji. must have a string following the command"""
+    """(i) turns text into emoji. must have a string following the command"""
     def toNum(x):
         return {0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}[x]
 
@@ -100,32 +93,31 @@ async def iconify(ctx, *args):
             icontext += toIconCode(char)
             icontext += ' '
         return icontext
-
-    await my_bot.delete_message(ctx.message)
+    iconstring = ''
     if args:
         args = ' '.join(args) # collapse ares to a string
         iconstring = args.lower()
-        return await my_bot.say(make(iconstring))
+        await my_bot.say(ctx.message.author.mention + ' ' + make(iconstring))
     else:
-        return await my_bot.say('`Error: must specify a string`')
+        await my_bot.say(ctx.message.author.mention + ' `Error: must specify a string`')
+    await my_bot.delete_message(ctx.message)
     print('iconify: ' + iconstring)
 
 @my_bot.command(pass_context=True, aliases=['c'])
 async def cat(ctx):
-    """(also c) random cat picture"""
+    """(c) random cat picture"""
     print('cat')
     async with aiohttp.ClientSession() as cs:
         async with cs.get('http://random.cat/meow') as r:
             res = await r.json()
     em = discord.Embed()
-    await my_bot.send_message(ctx.message.channel, content=None, embed=em.set_image(url=res['file']))
+    await my_bot.send_message(ctx.message.channel, content=ctx.message.author.mention, embed=em.set_image(url=res['file']))
     await my_bot.delete_message(ctx.message)
 
 @my_bot.command(pass_context=True, aliases=['d'])
 async def dog(ctx):
-    """(also d) random dog picture"""
+    """(d) random dog picture"""
     print('dog')
-    await my_bot.delete_message(ctx.message)
     isVideo = True
     while isVideo:
         async with aiohttp.ClientSession() as cs:
@@ -137,6 +129,45 @@ async def dog(ctx):
         else:
             isVideo = False
     em = discord.Embed()
-    await my_bot.send_message(ctx.message.channel, content=None, embed=em.set_image(url=res))
+    await my_bot.send_message(ctx.message.channel, content=ctx.message.author.mention, embed=em.set_image(url=res))
+    await my_bot.delete_message(ctx.message)
 
-my_bot.run('MzIxMDYxMjE4ODk3MTAwODAy.DBYjHw.IIk2oxT597yadkC0SCIbGMipg98') # Shibbot's Cousin (debate)
+
+class Shibs():
+    data = None
+    size = None
+    async def start(self):
+        async with aiohttp.ClientSession() as session:
+            url = 'https://api.github.com/repos/RobIsAnxious/shibesbot-db/contents/pictures'
+            with async_timeout.timeout(10):
+                auth = aiohttp.BasicAuth(config['github']['username'], password=config['github']['pass'])
+                async with session.get(url, auth=auth) as response:
+                    html = await response.text()
+            self.data = json.loads(html)
+            self.size = len(self.data)
+
+    def getAttr(self, num, attr):
+        return self.data[num][attr]
+
+    def getRand(self):
+        return randint(1, self.size)
+
+    def getImgUrl(self, num):
+        return self.data[num]['download_url']
+
+
+s = Shibs()
+my_bot.loop.run_until_complete(s.start())
+
+@my_bot.command(pass_context=True, aliases=['s'])
+async def shib(ctx):
+    """(s) basically hijacks the shibs database to provide shibs"""
+    n = s.getRand()
+    print('shib')
+    em = discord.Embed()
+    await my_bot.send_message(ctx.message.channel, content=ctx.message.author.mention, embed=em.set_image(url=s.getImgUrl(n)))
+    await my_bot.delete_message(ctx.message)
+
+
+
+my_bot.run(config['botkey']) # Shibbot's Cousin (debate)
